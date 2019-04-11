3c914f9c76f0d7e52950a1abb5074aecd8f4bfc9
Philip Flohr <philip.flohr@hadiko.dem>
2019-04-11 16:19:29 +0200

    restructure for easier use with ipython

--- plotter.py
+++ plotter.py
@@ -1,10 +1,7 @@
 import argparse
-import json
-import urllib.request
-from datetime import datetime
 import matplotlib.pyplot as plt
-import re
 
+from plotter_util import get_url, get_plot_data
 
 parser = argparse.ArgumentParser(description="An easy way to plot data from energy-charts.com using matplotlib...")
 parser.add_argument('--plot_lang', type=str, help="The plot language", default='en', choices=['en', 'de', 'fr', 'it'])
@@ -16,34 +13,9 @@ args = vars(parser.parse_args())
 
 plt.ylabel("Produced electricity in GW")
 
-url = 'https://energy-charts.de/power/'
-if args['plot_month']:
-    url += 'month_' + str(args['year']) + '_' + str(int(args['index'])).zfill(2) + '.json'
-else:
-    url += 'week_' + str(args['year']) + '_' + str(int(args['index'])).zfill(2) + '.json'
-
-with urllib.request.urlopen(url) as url_result:
-    data = json.loads(url_result.read().decode())
-    plot_data = []
-    plot_color = []
-    time_data = []
-    plot_labels = []
-    enabled_sources = [source for source in data if 'disabled' not in source]
-    for energy_source in enabled_sources:
-        source = energy_source['key'][0][args['plot_lang']]
-        plot_labels.append(source)
-        color_list = re.split('[\(\),]', energy_source['color'])
-        plot_color.append([int(s)/256 for s in color_list if s.isdigit()])
-        values = energy_source['values']
-        time_list = []
-        value_list = []
-        for [time, value] in values:
-            time_list.append(datetime.fromtimestamp(time/1000))
-            value_list.append(value)
-        time_data = time_list.copy()
-        plot_data.append(value_list)
-        time_list.clear()
-    plt.stackplot(time_data, plot_data, labels=plot_labels, colors=plot_color)
-    plt.legend()
-    plt.show()
+url = get_url(args['year'], int(args['index']), args['plot_month'])
+(time_data, plot_data, plot_labels, plot_color) = get_plot_data(url, args['plot_lang'])
+plt.stackplot(time_data, plot_data, labels=plot_labels, colors=plot_color)
+plt.legend()
+plt.show()
 
---
+++ plotter_util.py
@@ -0,0 +1,37 @@
+import json
+import urllib.request
+from datetime import datetime
+import re
+
+
+def get_plot_data(url, lang='en') -> ([],[],[],[]):
+    with urllib.request.urlopen(url) as url_result:
+        data = json.loads(url_result.read().decode())
+        plot_data = []
+        plot_color = []
+        time_data = []
+        plot_labels = []
+        enabled_sources = [source for source in data if 'disabled' not in source]
+        for energy_source in enabled_sources:
+            source = energy_source['key'][0][lang]
+            plot_labels.append(source)
+            color_list = re.split('[\(\),]', energy_source['color'])
+            plot_color.append([int(s) / 256 for s in color_list if s.isdigit()])
+            values = energy_source['values']
+            time_list = []
+            value_list = []
+            for [time, value] in values:
+                time_list.append(datetime.fromtimestamp(time / 1000))
+                value_list.append(value)
+            time_data = time_list.copy()
+            plot_data.append(value_list)
+    return time_data, plot_data, plot_labels, plot_color
+
+
+def get_url(year: int, index: int, plot_month: bool) -> str:
+    url = 'https://energy-charts.de/power/'
+    if plot_month:
+        url += 'month_' + str(year) + '_' + str(index).zfill(2) + '.json'
+    else:
+        url += 'week_' + str(year) + '_' + str(index).zfill(2) + '.json'
+    return url
