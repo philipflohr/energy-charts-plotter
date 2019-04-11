48747e8a4a888cf9d8acec55ec0c3094f71e3832
Philip Flohr <philip.flohr@hadiko.dem>
2019-04-11 15:39:58 +0200

    add colors and legend

--- plotter.py
+++ plotter.py
@@ -3,6 +3,7 @@ import json
 import urllib.request
 from datetime import datetime
 import matplotlib.pyplot as plt
+import re
 
 
 parser = argparse.ArgumentParser(description="An easy way to plot data from energy-charts.com using matplotlib...")
@@ -13,25 +14,25 @@ parser.add_argument('index', default=1, type=int, help="The index. (week n / mon
 args = vars(parser.parse_args())
 
 plt.ylabel("Produced electricity in GW")
-plt.legend()
 
 url = 'https://energy-charts.de/power/'
 if args['plot_month']:
-    pass
+    url += 'month_' + str(args['year']) + '_' + str(int(args['index'])).zfill(2) + '.json'
 else:
     url += 'week_' + str(args['year']) + '_' + str(int(args['index'])).zfill(2) + '.json'
-#rint(url)
 
 with urllib.request.urlopen(url) as url_result:
     data = json.loads(url_result.read().decode())
     plot_data = []
+    plot_color = []
     time_data = []
     plot_labels = []
     enabled_sources = [source for source in data if 'disabled' not in source]
     for energy_source in enabled_sources:
         source = energy_source['key'][0]['en']
         plot_labels.append(source)
-        color = energy_source['color']
+        color_list = re.split('[\(\),]', energy_source['color'])
+        plot_color.append([int(s)/256 for s in color_list if s.isdigit()])
         values = energy_source['values']
         time_list = []
         value_list = []
@@ -41,6 +42,7 @@ with urllib.request.urlopen(url) as url_result:
         time_data = time_list.copy()
         plot_data.append(value_list)
         time_list.clear()
-    plt.stackplot(time_data, plot_data, labels=plot_labels)
+    plt.stackplot(time_data, plot_data, labels=plot_labels, colors=plot_color)
+    plt.legend()
     plt.show()
 
