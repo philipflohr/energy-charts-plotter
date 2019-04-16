a45742df9a970dd17db5aa14f8813c1e5f7a6e95
Philip Flohr <philip.flohr@hadiko.de>
2019-04-16 13:44:29 +0200

    .

--- 1_week.py
+++ 1_week.py
@@ -16,9 +16,12 @@ ax.stackplot(time, data, labels=legend, colors=colors)
 ax.set_xlim(time[0], time[-1])
 #plt.xticks(rotation='vertical')
 plt.xticks(rotation=45)
+plt.grid(True)
+ax.set_facecolor('w')
+fig.suptitle("Produced electricity in Germany after fuels")
 ax.set_ylabel("Produced electricity in GW")
 ax.legend(loc='upper center', ncol=4, bbox_to_anchor=(0.5, -0.25))
 plt.savefig("test.pdf",format='pdf', bbox_inches='tight')
 
-print("Test")
+print("Source: " + url)
 
--- 1_week_wind_solar.py
+++ 1_week_wind_solar.py
@@ -1,27 +1,40 @@
 import matplotlib.pyplot as plt
 from plotter_util import *
 
+plt.style.use('seaborn')
+width = 345
+print(set_size(width))
+fig, (ax, ax2) = plt.subplots(1, 2, figsize=(6.3, 4))
+fig.suptitle("Produced electricity in Germany after fuels: Wind and Solar only")
+
 excluded_sources = ['Hydro Power', 'Biomass', 'Uranium', 'Brown Coal', 'Hard Coal', 'Gas', 'Others', 'Pumped Storage',
                     'Seasonal Storage', 'Oil']
 
-url = get_url(2018,4,False)
+url = get_url(2018, 4, False)
 time, data, legend, colors = get_plot_data(url, excluded_sources=excluded_sources)
 
-plt.style.use('seaborn')
-width = 345
-
-# Initialise figure instance
-print(set_size(width))
-fig, ax = plt.subplots(1, 1, figsize=(6.3, 4))
-
-# Plot
 ax.stackplot(time, data, labels=legend, colors=colors)
 ax.set_xlim(time[0], time[-1])
-plt.xticks(rotation=45)
-fig.suptitle("Electricity produced in Germany: Wind and Solar only")
+ax.set_ylim(0, 42)
+ax.set_facecolor('w')
 ax.set_ylabel("Produced electricity in GW")
-ax.legend(loc='upper center', ncol=4, bbox_to_anchor=(0.5, -0.25))
-plt.savefig("test.pdf",format='pdf', bbox_inches='tight')
+for tick in ax.get_xticklabels():
+    tick.set_rotation(45)
+#ax.legend(loc='upper center', ncol=4, bbox_to_anchor=(0.5, -0.25))
+
+url = get_url(2018, 34, False)
+time, data, legend, colors = get_plot_data(url, excluded_sources=excluded_sources)
+
+ax2.stackplot(time, data, labels=legend, colors=colors)
+ax2.set_xlim(time[0], time[-1])
+ax2.set_ylim(0, 42)
+ax2.set_facecolor('w')
+plt.figlegend(loc='upper center', ncol=4, bbox_to_anchor=(0.5, -0.25))
+for tick in ax2.get_xticklabels():
+    tick.set_rotation(45)
+
+plt.grid(True)
+plt.savefig("test.pdf", format='pdf', bbox_inches='tight')
 
 print("Test")
 
