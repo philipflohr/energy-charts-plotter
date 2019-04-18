c35cf954a3ffdca60161f542e32f43b2552ce71b
Philip Flohr <philip.flohr@hadiko.dem>
2019-04-18 16:20:33 +0200

    .

--- 1_week_wind_solar.py
+++ 1_week_wind_solar.py
@@ -4,7 +4,8 @@ from plotter_util import *
 plt.style.use('seaborn')
 width = 345
 print(set_size(width))
-fig, (ax, ax2) = plt.subplots(1, 2, figsize=(6.3, 4))
+#plt.xkcd()
+fig, (ax, ax2) = plt.subplots(1, 2, figsize=(6.3, 4), sharex=False, sharey=True)
 fig.suptitle("Produced electricity in Germany after fuels: Wind and Solar only")
 
 excluded_sources = ['Hydro Power', 'Biomass', 'Uranium', 'Brown Coal', 'Hard Coal', 'Gas', 'Others', 'Pumped Storage',
@@ -14,7 +15,7 @@ url = get_url(2018, 4, False)
 time, data, legend, colors = get_plot_data(url, excluded_sources=excluded_sources)
 _, usage = get_usage(url)
 
-ax.plot(time, usage, label='usage', color='r')
+ax.plot(time, usage, label='Energy usage', color='r')
 ax.stackplot(time, data, labels=legend, colors=colors)
 ax.set_xlim(time[0], time[-1])
 ax.set_ylim(0, 80)
@@ -22,7 +23,7 @@ ax.set_facecolor('w')
 ax.set_ylabel("Produced electricity in GW")
 for tick in ax.get_xticklabels():
     tick.set_rotation(45)
-#ax.legend(loc='upper center', ncol=4, bbox_to_anchor=(0.5, -0.25))
+ax.legend(loc='upper center', ncol=4, bbox_to_anchor=(0.5, -0.25))
 
 url = get_url(2018, 34, False)
 time, data, legend, colors = get_plot_data(url, excluded_sources=excluded_sources)
@@ -33,7 +34,10 @@ ax2.stackplot(time, data, labels=legend, colors=colors)
 ax2.set_xlim(time[0], time[-1])
 ax2.set_ylim(0, 80)
 ax2.set_facecolor('w')
-plt.figlegend(loc='upper center', ncol=4, bbox_to_anchor=(0.5, -0.25))
+ax2.yaxis.set_label_position("right")
+ax2.set_ylabel("Produced electricity in GW")
+
+#plt.figlegend(loc='upper center', ncol=4, bbox_to_anchor=(0.5, -0.25))
 for tick in ax2.get_xticklabels():
     tick.set_rotation(45)
 
